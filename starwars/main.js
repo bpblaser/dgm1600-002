b8b6e007d3240aba71884541b76d96ab92cb9099
bpblaser <bpblaser12@gmail.com>
2020-02-28 15:59:40 -0700

    Styling SW

--- starwars/main.js
+++ starwars/main.js
@@ -1,15 +1,50 @@
-import { films } from '../data/films'
-
-console.log('Hello there. General Kenobi, you are a bold one.')
-
-//console.log(document.querySelector(".greeting"))
+import { films } from '../data/films.js'
+import { people } from '../data/people.js'
+import { starships } from '../data/starships.js'
 
 const greetingDiv = document.querySelector('.greeting')
+const maleButton = document.querySelector('maleButton')
+const femaleButton = document.querySelector('femaleButton')
+const otherButton = document.querySelector('otherButton')
 
-greetingDiv.textContent = 'I just inserted text into a DOM element using my nutty JS skills'
+const maleCharacters = people.filter(person => person.gender === "male")
+console.log(maleCharacters)
 
-console.log(greetingDiv.textContent)
+const femaleCharacters = people.filter(person => person.gender === "female")
+console.log(femaleCharacters)
 
-console.log(films)
+const otherCharacters = people.filter(person => {
+  if (person.gender === "hermaphrodite" || person.gender === "n/a") {
+  return person
+}
+})
+console.log(otherCharacters)
 
-greetingDiv.textContent = films[0].opening_crawl
+let counter = 1
+
+people.forEach(person => {
+
+let anchorWrap = document.createElement("a")
+anchorWrap.href = "#"
+
+let imageItem = document.createElement("img")
+imageItem.src = `https://starwars-visualguide.com/assets/img/characters/${counter}.jpg`
+
+imageItem.addEventListener('error', (event) => {
+	//console.log(`${event.type}: Loading image\n`)
+	//console.log(event)
+	imageItem.hidden = true
+	//imageItem.src = ‘../images/uvu-logo.jpg 
+})
+//add some way to handle user clicks on the image
+imageItem.addEventListener("click", (event) => {
+	console.log(event)
+})
+anchorWrap.appendChild(imageItem)
+greetingDiv.appendChild(anchorWrap)
+counter++
+})
+
+maleButton.addEventListener("click"), (event) => {
+console.log
+}

8f98b8abccf74bcfe9db7aefdfe13caf2fe67c1b
bpblaser <bpblaser12@gmail.com>
2020-03-10 23:34:29 -0600

    Created utils.js

---
+++ utils.js
@@ -0,0 +1,11 @@
+export.function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+    
+  }
+  //
+  function removeChildren(element) {
+    while (element.firstChild) {
+      element.removeChild(element.firstChild);
+    }
+  }
